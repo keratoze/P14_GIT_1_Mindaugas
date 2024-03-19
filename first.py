1a2cd282e1073e0857bd20034f74ae774228c92f
mindaugas <martinelis.mindaugas@gmail.com>
2024-03-19 09:24:32 +0200

    Meniu ir pasirinkimu pakeitimai
    pasalintas antras meniu pasirinkimas
    redaguotas pasirinkimu tikrinimas ir printas
    po tikrinimo

--- first.py
+++ first.py
@@ -1,11 +1,11 @@
 #suformuosime nedideli pasirinkimu meniu vartotojui
 while True:
     print('\n1. Atspauzdinti ivesti atbulai'
-          '\n2. Atspauzdinti ivesti ir simboliu ivestije kieki'
           '\nq - iseiti ')
     pasirinkimas = input('>>>>')
 #####################
  #pasirinkimo patikrinimas
-    if pasirinkimas not in ('1','2','q'):
+    if pasirinkimas not in ('1','q'):
+        print('===========')
         print('Tokio pasirinkimo nera')
         continue
